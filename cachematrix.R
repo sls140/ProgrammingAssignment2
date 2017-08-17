5f514c9c8636e9786113f436fce04dba9ac1b10e
sls140 <sls140@gmail.com>
2017-08-17 10:47:16 +0900

    Update cachematrix.R
    This class is advertised as "for beginners". How infuriatingly misleading. It certainly could be, if it were actually taught that way, rather than consisting of lectures filled with details that are strangely both incomprehensible to new learners and yet, I suspect, redundantly obvious to anyone with sufficient knowledge of R to actually understand them.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## These two functions are designed to cache the inverse of a matrix and later load the 
+## cached inverse rather than processing processing it anew.
+## This first function caches the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) inv <<- solve
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This second function should compute the inverse of the special
+    "matrix" returned by `makeCacheMatrix`. If the inverse has
+    already been calculated (and the matrix has not changed), then
+    `cacheSolve` should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
